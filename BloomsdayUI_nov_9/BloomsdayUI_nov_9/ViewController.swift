921c2cba755dd57413cd8a0f625fbfde7b801642
chauncyc <ccullitan@zagmail.gonzaga.edu>
2015-11-09 19:47:34 -0800

    msg

---
+++ BloomsdayUI_nov_9/BloomsdayUI_nov_9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BloomsdayUI_nov_9
+//
+//  Created by Jason Schnagl on 11/9/15.
+//  Copyright © 2015 SeniorDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
